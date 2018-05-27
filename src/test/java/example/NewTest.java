e9312600f068634d0c6b94ac8faa4ae8ef26e4a2
Shaad Khan <shaad39@gmail.com>
2018-05-27 20:52:28 +0530

    this a test for java
    Change-Id: Id8bc4d0e44d115381bd08705541262592b87b091
    Signed-off-by: Shaad Khan <shaad39@gmail.com>

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,31 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+//import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;		
+import org.testng.Assert;	
+public class NewTest {
+	private WebDriver driver;	
+  @Test
+  public void f() {
+	  
+	  driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\shadab.khan\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+	  driver = new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+  }
+
+}
